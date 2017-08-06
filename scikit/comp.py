a53198f1a264dc94fc6cdd4504070f2393c4fe29
mohitkumar <ch.mohitkumar89@gmail.com>
2017-08-06 13:19:45 +0530

    comp- gradient boost

--- scikit/comp.py
+++ scikit/comp.py
@@ -1,15 +1,18 @@
 from __future__ import print_function
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier, VotingClassifier, ExtraTreesClassifier, RandomForestClassifier
 from sklearn.model_selection import cross_val_score, cross_val_predict,train_test_split, GridSearchCV
 from sklearn.metrics import confusion_matrix, roc_auc_score
 from sklearn import metrics
 import numpy as np
-import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.feature_selection import SelectFromModel
 from sklearn.externals import joblib
 
-train = pd.read_csv('/home/mohit/comp_data/train.csv')
+train = pd.read_csv('/home/mohit/comp_data/train1.csv')
+
 
 train['siteid'].fillna(-999, inplace=True)
 train['browserid'].fillna("None", inplace=True)
@@ -17,7 +20,6 @@ train['devid'].fillna("None", inplace=True)
 train['datetime'] = pd.to_datetime(train['datetime'])
 train['tweekday'] = train['datetime'].dt.weekday
 train['thour'] = train['datetime'].dt.hour
-#train['tminute'] = train['datetime'].dt.minute
 
 cols = ['siteid','offerid','category','merchant','countrycode','browserid','devid']
 
@@ -27,25 +29,33 @@ for col in cols:
     train[col] = lbl.transform(list(train[col].values))
 
 cols_to_use = list(set(train.columns) - set(['ID','datetime','click']))
+print(cols_to_use)
 train = train.sample(frac=1).reset_index(drop=True)
+
+print(train.describe())
 
 X_train, X_test, y_train, y_test = train_test_split(train[cols_to_use], train['click'], test_size=0.2)
 
 
-def explore_data_hist():
-    train.hist(bins=50, figsize=(20,15))
-    plt.show()
-
-
-def explore_data_relation():
-    train.groupby('click').hist()
-    print(train.corr())
-    pd.scatter_matrix(train, alpha=0.3, figsize=(14, 8), diagonal='kde');
-    plt.show()
-
-
 def classify():
-    clf = RandomForestClassifier(n_estimators=15)
+    gb_grid_params = {'learning_rate': [0.1, 0.05, 0.02, 0.01],
+                      'max_depth': [4, 6, 8],
+                      'min_samples_leaf': [20, 50, 100, 150],
+                      'n_estimators':[100,300,500]
+                      }
+
+    clf1 = GradientBoostingClassifier(criterion='friedman_mse', init=None,
+              learning_rate=0.05, loss='deviance', max_depth=6,
+              max_features=None, max_leaf_nodes=None,
+              min_impurity_split=1e-07, min_samples_leaf=50,
+              min_samples_split=2, min_weight_fraction_leaf=0.0,
+              n_estimators=300, presort='auto', random_state=None,
+              subsample=1.0, verbose=True, warm_start=False)
+    clf = Pipeline([
+        ('feature_selection', SelectFromModel(LinearSVC(penalty="l1", dual=False))),
+        ('classification', clf1)
+    ])
+    #grid = GridSearchCV(estimator=clf, param_grid=gb_grid_params, n_jobs=2)
     clf.fit(X_train, y_train)
     preds = cross_val_predict(clf,X_train, y_train, cv=3)
     print(confusion_matrix(y_train, preds))
@@ -61,10 +71,7 @@ def predict(clf):
     print(roc_auc_score(y_test,predicted))
 
 
-
 if __name__ == '__main__':
     clf = classify()
-    #predict(clf)
-    joblib.dump(clf, 'comp.pkl')
-    #explore_data_hist()
-    #explore_data_relation()
+    predict(clf)
+    #joblib.dump(clf, 'comp_ada.pkl')
