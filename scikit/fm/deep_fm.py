35abbf9c2b274c0bf2c37d2730f8a7154da2c09e
mohitkumar <ch.mohitkumar89@gmail.com>
2017-08-11 11:13:42 +0530

    deep fm

---
+++ scikit/fm/deep_fm.py
@@ -0,0 +1,159 @@
+import numpy as np
+
+import tensorflow as tf
+
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_extraction import FeatureHasher
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import datasets
+from sklearn.metrics import roc_auc_score, f1_score, confusion_matrix
+from model import get_dummy, get_data_vect, get_data_out
+
+def make_embeddings(x, rank, num_features, depth=1, seed=12345):
+    """
+      assumes that all hidden layers are width `rank`
+    """
+    assert depth > 0
+    V = tf.Variable(tf.truncated_normal([rank, num_features], stddev=0.2, mean=0, seed=seed), name="v_1")
+    #10x5
+    b = tf.Variable(tf.truncated_normal([rank, 1], stddev=0.2, mean=0, seed=seed), name="b_1")
+    #10x1
+    Vx = tf.nn.relu(tf.matmul(V, x) + b)
+    #10x999
+    for i in range(depth - 1):
+        V = tf.Variable(tf.truncated_normal([rank, rank], stddev=0.2, mean=0, seed=seed), name="v_%s" % i)
+        b = tf.Variable(tf.truncated_normal([rank, 1], stddev=0.2, mean=0, seed=seed), name="b_%s" % i)
+        Vx = tf.nn.relu(tf.matmul(V, Vx) + b)
+
+    return Vx
+
+
+def factorize(observed_features,
+              labels,
+              observed_features_validation,
+              labels_validation,
+              rank,
+              max_iter=100,
+              verbose=False,
+              lambda_v=0,
+              lambda_k=0,
+              lambda_w=0,
+              lambda_constants=0,
+              epsilon=0.001,
+              optimizer=tf.train.AdamOptimizer(),
+              depth=3,
+              seed=12345):
+    # Extract info about shapes etc from the training data
+    num_items = observed_features.shape[0]
+    num_features = observed_features.shape[1]
+
+    # matrix defining the inner product weights when doing interactions
+    K = tf.Variable(tf.truncated_normal([rank, rank], stddev=0.2, mean=0, seed=seed), name="metric_matrix")
+    #10x10
+    # coefficients for linear function on inputs (wide part)
+    w = tf.Variable(tf.truncated_normal([1, num_features], stddev=0.2, mean=0, seed=seed), name="hyperplane")
+    #1x5
+    # coefficients for linear functinos on inputs (deep part)
+    lw = tf.Variable(tf.truncated_normal([1, rank], stddev=0.2, mean=0, seed=seed), name="latenthyperplane")
+    #1x10
+    # bias in linear function
+    b = tf.Variable(tf.truncated_normal([1, 1], stddev=0.2, mean=0, seed=seed), name="b_one")
+    #1x1
+    x = tf.placeholder(tf.float32, [None, num_features])
+    y = tf.placeholder(tf.float32)
+
+    norm_x = tf.nn.l2_normalize(x, dim=0)
+    #999x5
+    Vx = make_embeddings(tf.transpose(norm_x), rank, num_features, depth=depth, seed=seed)
+    #10x999
+    right_kern = tf.matmul(K, Vx)
+    #10x999
+    full_kern = tf.matmul(tf.transpose(Vx), right_kern)
+    #999x999
+    linear = tf.matmul(w, tf.transpose(norm_x))
+    #1x999
+    latent_linear = tf.matmul(lw, Vx)
+    #1x999
+
+    pred = tf.reduce_sum(tf.sigmoid(linear + latent_linear + full_kern + b))
+    #999
+    # todo: dropout. currently no regularization on the interaction layers in the cost functino
+    # can handle with FTRL optimization
+    cost = tf.reduce_mean(-y * tf.log(pred + 0.0000000001) - (1 - y) * tf.log((1 - pred + 0.0000000001)) +
+                          lambda_k * tf.nn.l2_loss(K) +
+                          lambda_w * tf.nn.l2_loss(w) +
+                          lambda_constants * tf.nn.l2_loss(b))
+    optimize = optimizer.minimize(cost)
+    norm = tf.reduce_mean(tf.nn.l2_loss(w))
+    saver = tf.train.Saver()
+    init = tf.initialize_all_variables()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        last_cost = 1000000
+        for iter in range(0, max_iter):
+            avg_cost = 0
+
+            for i in range(num_items):
+                _, c, n = sess.run([optimize, cost, norm],
+                                   feed_dict={x: observed_features[i].reshape(1, num_features), y: labels[i]})
+                avg_cost += c / num_items
+            if verbose:
+                print("epoch: %s, cost: %s" % (iter + 1, avg_cost))
+
+            # check for convergence
+            if abs(avg_cost - last_cost) / avg_cost < epsilon:
+                break
+
+            last_cost = avg_cost
+
+        if verbose:
+            print("optimization finished")
+        predictions = []
+        total_costs = 0
+        for i in range(observed_features_validation.shape[0]):
+            p, c = sess.run([pred, cost], feed_dict={x: observed_features_validation[i].reshape(1, num_features),
+                                                     y: labels_validation[i]})
+            predictions.append(p)
+            total_costs += c
+        saver.save(sess, 'deep_comp')
+        #out_pred = sess.run(pred, feed_dict={x:get_data_out()})
+        #print out_pred
+        return predictions, total_costs / observed_features_validation.shape[0], sess.run([norm])
+
+def predict_probab(X_test, rank, seed, depth):
+    num_features = X_test.shape[1]
+
+    K = tf.Variable(tf.truncated_normal([rank, rank], stddev=0.2, mean=0, seed=seed), name="metric_matrix")
+    w = tf.Variable(tf.truncated_normal([1, num_features], stddev=0.2, mean=0, seed=seed), name="hyperplane")
+    lw = tf.Variable(tf.truncated_normal([1, rank], stddev=0.2, mean=0, seed=seed), name="latenthyperplane")
+    b = tf.Variable(tf.truncated_normal([1, 1], stddev=0.2, mean=0, seed=seed), name="b_one")
+    x = tf.placeholder(tf.float32, [None, num_features])
+    y = tf.placeholder(tf.float32)
+
+    norm_x = tf.nn.l2_normalize(x, dim=0)
+    Vx = make_embeddings(tf.transpose(norm_x), rank, num_features, depth=depth, seed=seed)
+    right_kern = tf.matmul(K, Vx)
+    full_kern = tf.matmul(tf.transpose(Vx), right_kern)
+    linear = tf.matmul(w, tf.transpose(norm_x))
+    latent_linear = tf.matmul(lw, Vx)
+
+    pred = tf.reduce_sum(tf.sigmoid(linear + latent_linear + full_kern + b))
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        out_pred = []
+        saver.restore(sess, 'deep_comp')
+        for i in range(X_test.shape[0]):
+            p= sess.run(pred, feed_dict={x: X_test[i].reshape(1, num_features),y: 1})
+            out_pred.append(round(p,3))
+        print out_pred
+
+if __name__ == '__main__':
+    #X_train, X_valid, Y_train, Y_valid = get_data_vect('/home/mohit/comp_data/train1.csv')
+    r = 10
+    #predictions, test_costs, norm = factorize(X_train, Y_train, X_valid, Y_valid, r, verbose=True, lambda_v=0.1,max_iter=300)
+    #print("rank: %s, cost: %s, overall AUC: %s, norm: %s") % (
+    #r, test_costs, roc_auc_score(Y_valid, predictions, average="weighted"), norm)
+
+    predict_probab(get_data_out(),r,12345,3)
--- scikit/fm/model.py
+++ scikit/fm/model.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, roc_auc_score
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.feature_extraction import DictVectorizer as DV
+from sklearn.datasets import make_classification
 
 def get_test(csv_file):
     train = pd.read_csv(csv_file)
@@ -45,6 +46,27 @@ def get_data(csv_file):
     X_train, X_valid, Y_train, Y_valid = train_test_split(train[cols_to_use], train['click'], test_size=0.2, random_state=2017)
     return np.array(X_train), np.array(X_valid), np.array(Y_train), np.array(Y_valid)
 
+def get_data_out():
+    train = pd.read_csv('/home/mohit/comp_data/test.csv')
+    train['siteid'].fillna(-999, inplace=True)
+    train['browserid'].fillna("None", inplace=True)
+    train['devid'].fillna("None", inplace=True)
+    train['datetime'] = pd.to_datetime(train['datetime'])
+    train['tweekday'] = train['datetime'].dt.weekday
+    train['thour'] = train['datetime'].dt.hour
+    train['tminute'] = train['datetime'].dt.minute
+    num_train = train.drop(
+        ['ID', 'datetime', 'siteid', 'offerid', 'category', 'merchant', 'countrycode', 'browserid', 'devid'],
+        axis=1)
+    cat_train = train.drop(['ID', 'datetime','tweekday', 'thour', 'tminute'], axis=1)
+    cat_train['siteid'] = cat_train['siteid'].astype(int)
+
+    x_cat_train = cat_train.to_dict(orient='records')
+    vectorizer = DV(sparse=False, dtype=np.int32)
+    vec_x_cat_train = vectorizer.fit_transform(x_cat_train)
+    train = np.hstack((num_train, vec_x_cat_train))
+    return np.array(train[0:5,:])
+
 
 def get_data_vect(csv_file):
     train = pd.read_csv(csv_file)
@@ -77,8 +99,14 @@ def print_progress(session, epoch, feed_dict_train, feed_dict_validate, val_loss
     print(msg.format(epoch + 1, acc, val_acc, val_loss))
 
 
+def get_dummy():
+    X, Y = make_classification(n_samples=1000, n_features=4)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+    return np.array(X_train), np.array(X_test), np.array(y_train), np.array(y_test)
+
+
 if __name__ == '__main__':
-    X_train, X_valid, Y_train, Y_valid = get_data('/home/mohit/comp_data/train2.csv')
+    X_train, X_valid, Y_train, Y_valid = get_dummy()#get_data_vect('/home/mohit/comp_data/train2.csv')
     Y_valid_orig = Y_valid.copy()
     Y_train = Y_train.reshape(-1)
     Y_valid = Y_valid.reshape(-1)
@@ -86,7 +114,7 @@ if __name__ == '__main__':
     Y_train = np.eye(2)[Y_train]
     Y_valid = np.eye(2)[Y_valid]
     n, p = X_train.shape
-    k = 5
+    k = 1000
     X = tf.placeholder('float', shape=[None, p])
     y = tf.placeholder('float', shape=[None, 2])
     y_true_cls = tf.argmax(y, dimension=1)
@@ -95,14 +123,16 @@ if __name__ == '__main__':
     W = tf.Variable(tf.random_normal((1,p), stddev=0.1))
 
     V = tf.Variable(tf.random_normal([k, p], stddev=0.01))
-    linear_terms = tf.add(w0, tf.reduce_sum(tf.multiply(W, X), 1, keep_dims=True))
+    linear_terms = tf.add(w0, tf.reduce_sum(tf.multiply(W, X), axis=1, keep_dims=True))
+    #linear_terms = tf.nn.sigmoid(linear_terms)
     interactions = (tf.multiply(0.5,
                                 tf.reduce_sum(
                                     tf.subtract(
                                         tf.pow(tf.matmul(X, tf.transpose(V)), 2),
                                         tf.matmul(tf.pow(X, 2), tf.transpose(tf.pow(V, 2)))),
                                     1, keep_dims=True)))
-    y_hat = tf.add(linear_terms, interactions)
+
+    y_hat = tf.add(linear_terms,interactions)
     #y_hat = tf.nn.sigmoid(y_hat)
     lambda_w = tf.constant(0.001, name='lambda_w')
     lambda_v = tf.constant(0.001, name='lambda_v')
@@ -112,22 +142,16 @@ if __name__ == '__main__':
             tf.multiply(lambda_w, tf.pow(W, 2)),
             tf.multiply(lambda_v, tf.pow(V, 2)))))
 
-    error = tf.nn.softmax_cross_entropy_with_logits(logits=y_hat,labels=y)
+    cost = tf.reduce_mean(tf.add(tf.nn.softmax_cross_entropy_with_logits(logits=y_hat,labels=y),
+                                 l2_norm))
 
-    y_pred = tf.nn.softmax(y_hat)
-    y_pred_cls = tf.argmax(y_hat, dimension=1)
-    correct_prediction = tf.equal(y_pred_cls, y_true_cls)
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    optimizer = tf.train.AdadeltaOptimizer(.0001).minimize(cost)
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y_hat, dimension=1), y_true_cls), tf.float32))
 
-    loss = tf.add(error, l2_norm)
-    cost = tf.reduce_mean(loss)
-    eta = tf.constant(0.001)
-    optimizer = tf.train.AdagradOptimizer(eta).minimize(cost)
     N_EPOCHS = 10000
     # Launch the graph.
     saver = tf.train.Saver()
     init = tf.global_variables_initializer()
-    msg = "Epoch {0} --- Validation Loss: {3:.3f}"
     with tf.Session() as sess:
         sess.run(init)
         for epoch in range(N_EPOCHS):
@@ -144,11 +168,11 @@ if __name__ == '__main__':
 
         print('Loss: ', sess.run(cost, feed_dict={X: x_data, y: y_data}))
         print('accuracy:', sess.run(accuracy, feed_dict={X: x_data, y: y_data}))
-        preds = sess.run(y_pred_cls, feed_dict={X: X_valid})
+        preds = sess.run(tf.argmax(y_hat, dimension=1), feed_dict={X: X_valid})
         preds_prob = sess.run(tf.nn.softmax(y_hat), feed_dict={X: X_valid})
         #print('Predictions:', preds)
         #print('probs:', preds_prob)
-        print np.count_nonzero(preds == 0)
+        #print np.count_nonzero(preds == 0)
         print confusion_matrix(Y_valid_orig, preds)
         print roc_auc_score(Y_valid_orig,preds)
         #saver.save(sess, 'fm_comp')
